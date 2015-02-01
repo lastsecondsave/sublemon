d17b5708f13df6428c31e2768f29a0b590f17a17
lastsecondsave <lastsecondsave@outlook.com>
2015-02-01 19:05:31 +0300

    Build engined updates

--- Sublemon/plugins.py
+++ Sublemon/plugins.py
@@ -4,6 +4,7 @@ class ExecPowershellCommand(sublime_plugin.WindowCommand):
     def run(self, **args):
         if "shell_cmd" in args:
             if "cmd" not in args:
-                args["cmd"] = ["powershell.exe", "-Command", args["shell_cmd"]]
+                args["cmd"] = ["powershell.exe",
+                        "-Command", "sublbuild;" + args["shell_cmd"]]
             del args["shell_cmd"]
         self.window.run_command("exec", args)
