27192c4e0c3fb924be4d6d170bb3cceaa17472f4
lastsecondsave <lastsecondsave@outlook.com>
2015-03-17 23:06:35 +0300

    Cleanup in powershell's exec
     - Parameter 'load_profile' to use system profile (false by default).
     - 'cmd' parameter is now used.

--- Sublemon PowerShell/powershell_exec.py
+++ Sublemon PowerShell/powershell_exec.py
@@ -1,15 +1,27 @@
 import os, sublime, sublime_plugin
 
 class PowershellExecCommand(sublime_plugin.WindowCommand):
-
   def run(self, **args):
-    if not "shell_cmd" in args or "cmd" in args:
-      self.window.run_command("exec", args)
+    if "shell_cmd" in args:
+      command = [args["shell_cmd"]]
+      del args["shell_cmd"]
+    elif "cmd" in args:
+      command = args["cmd"]
+    else:
+      return False
+
+    load_profile = False
+    if "load_profile" in args:
+      load_profile = args["load_profile"]
+      del args["load_profile"]
 
     script = os.path.join(sublime.packages_path(), "Sublemon PowerShell", "powershell_exec.ps1")
-    command = args["shell_cmd"]
 
-    args["cmd"] = ["powershell.exe",
-        "-ExecutionPolicy", "Unrestricted", "-File", script, "-Command", command]
-    del args["shell_cmd"]
+    cmd = ["powershell.exe"]
+    if not load_profile:
+      cmd.append("-NoProfile")
+    cmd.extend(["-ExecutionPolicy", "Unrestricted", "-File", script, "-Command"])
+    cmd.extend(command)
+
+    args["cmd"] = cmd
     self.window.run_command("exec", args)
