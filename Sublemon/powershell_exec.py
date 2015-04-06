b9efcf12a75a8d4bf940915d732fe45e3bae7582
lastsecondsave <lastsecondsave@outlook.com>
2015-04-06 22:54:11 +0300

    PowerShell package updates
     - Moved inside main's directory.
     - Temporal stub for syntax.
     - 'Context' option for build.

---
+++ Sublemon/powershell_exec.py
@@ -0,0 +1,39 @@
+import os, sublime, sublime_plugin
+
+class PowershellExecCommand(sublime_plugin.WindowCommand):
+  def run(self, **args):
+    if "shell_cmd" in args:
+      command = [args["shell_cmd"]]
+      del args["shell_cmd"]
+    elif "cmd" in args:
+      command = args["cmd"]
+    else:
+      return False
+
+    load_profile = self.get_argument("load_profile", args)
+    context = self.get_argument("context", args)
+
+    script = os.path.join(sublime.packages_path(), "Sublemon", "powershell", "powershell_exec.ps1")
+
+    cmd = ["powershell.exe", "-ExecutionPolicy", "Unrestricted"]
+
+    if not load_profile:
+      cmd.append("-NoProfile")
+
+    cmd.extend(["-File", script])
+
+    if context:
+      cmd.extend(["-Context", os.path.join(sublime.packages_path(), context)])
+
+    cmd.append("-Command")
+    cmd.extend(command)
+
+    args["cmd"] = cmd
+    self.window.run_command("exec", args)
+
+  def get_argument(self, name, args):
+    result = None
+    if name in args:
+      result = args[name]
+      del args[name]
+    return result
