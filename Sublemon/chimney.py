904bb827c3c70aba79af7aee8ce35bf6f4b8e439
lastsecondsave <lastsecondsave@outlook.com>
2016-01-11 12:30:05 +0300

    Chimney - custom exec logic

---
+++ Sublemon/chimney.py
@@ -0,0 +1,233 @@
+import collections
+import os
+import sublime, sublime_plugin
+import subprocess
+import sys
+import threading
+
+## UTILS ##
+
+def _get_startupinfo():
+    startupinfo = None
+    if sys.platform == "win32":
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    return startupinfo
+
+## CLASSES ##
+
+class Pipe:
+    def output(self, line):
+        self.next_pipe.output(line)
+
+    def error(self, line):
+        self.next_pipe.error(line)
+
+    def flush(self):
+        pass
+
+    def connect(self, next_pipe):
+        self.next_pipe = next_pipe
+
+class LineBuffer:
+    def __init__(self, consumer, encoding=None):
+        self.buff = ""
+        self.encoding = encoding or sys.stdin.encoding
+        self.consumer = consumer
+
+    def next(self, chunk):
+        try:
+            chunk = chunk.decode(self.encoding)
+        except:
+            chunk = "[DECODE ERROR]"
+
+        b = 0
+        e = chunk.find('\n')
+
+        while e != -1:
+            cutoff = chunk[b:e]
+            if len(cutoff) > 0 and cutoff[-1] == '\r':
+                cutoff = cutoff[:-1]
+
+            self.buff += cutoff
+            self.flush()
+
+            b = e + 1
+            e = chunk.find('\n', b)
+
+        self.buff = chunk[b:]
+
+    def flush(self):
+        if len(self.buff) > 0:
+            self.consumer(self.buff)
+            self.buff = ""
+
+class OutputPanel:
+    def __init__(self, window):
+        self.window = window
+        self.view = window.create_output_panel("exec")
+
+        self.line_buffer_lock = threading.Lock()
+        self.line_buffer = collections.deque()
+
+        self.set_settings(
+            line_numbers=False,
+            gutter=False,
+            scroll_past_end=False,
+            result_file_regex="",
+            result_line_regex=""
+        )
+
+    def reset(self):
+        self.view.run_command('erase_view')
+
+        with self.line_buffer_lock:
+            self.line_buffer.clear()
+
+        self.set_settings(
+            syntax="Packages/Text/Plain text.tmLanguage"
+        )
+
+    def set_settings(self, syntax=None, **settings):
+        for k, v in settings.items():
+            if v:
+                self.view.settings().set(k, v)
+
+        if syntax:
+            self.view.assign_syntax(syntax)
+
+    def append_line(self, line):
+        with self.line_buffer_lock:
+            reinvalidate = len(self.line_buffer) == 0
+            self.line_buffer.append(line)
+
+        if reinvalidate:
+            self.invalidate()
+
+    def invalidate(self):
+        sublime.set_timeout(self.paint, 0)
+
+    def paint(self):
+        with self.line_buffer_lock:
+            if len(self.line_buffer) == 0:
+                return
+            characters = '\n'.join(self.line_buffer) + '\n'
+            self.line_buffer.clear()
+
+        self.view.run_command('append', {'characters': characters, 'force': True, 'scroll_to_end': True})
+
+    def show(self):
+        self.window.run_command("show_panel", {"panel": "output.exec"})
+
+class OutputPanelPipe(Pipe):
+    def __init__(self, outputPanel):
+        self.outputPanel = outputPanel
+
+    def output(self, line):
+        self.outputPanel.append_line(line)
+
+    def error(self, line):
+        self.outputPanel.append_line(line)
+
+class AsyncStreamConsumer(threading.Thread):
+    def __init__(self, stream, consumer):
+        super().__init__(self)
+        self.stream = stream
+        self.consumer = consumer
+
+    def run(self):
+        while True:
+            chunk = os.read(self.stream.fileno(), 2**15)
+            if len(chunk) == 0:
+                break
+            self.consumer.next(chunk)
+        self.consumer.flush()
+        self.stream.close()
+
+class Executor:
+    def __init__(self, window):
+        self.window = window
+        self.output = OutputPanel(window)
+        print("Created executor for #" + str(window.id()))
+
+    def run(self, **options):
+        option = lambda x: self.get_option(x, options)
+
+        shell_cmd = option("shell_cmd")
+        cmd = option("shell_cmd")
+
+        if shell_cmd and sys.platform == "win32":
+            pass
+        elif shell_cmd:
+            cmd = [os.environ["SHELL"], "-c", shell_cmd]
+
+        print("Running " + shell_cmd if shell_cmd else " ".join(cmd))
+
+        proc = subprocess.Popen(cmd, startupinfo=_get_startupinfo(),
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.DEVNULL)
+
+        # Prepare output panel
+
+        self.output.reset()
+        output_settings = dict()
+        self.copy_output_settings(options, output_settings)
+        self.output.set_settings(**output_settings)
+
+        if not self.get_option('no_output', options):
+            self.output.show()
+
+        # Start pipes
+
+        pipe = OutputPanelPipe(self.output)
+        AsyncStreamConsumer(proc.stdout, LineBuffer(pipe.output)).start()
+        AsyncStreamConsumer(proc.stderr, LineBuffer(pipe.error)).start()
+
+    def copy_output_settings(self, options, output_settings):
+        copy = lambda x, y: self.copy_output_setting(x, y, options, output_settings)
+
+        copy("syntax", "syntax")
+        copy("file_regex", "result_file_regex")
+        copy("line_regex", "result_line_regex")
+
+    def copy_output_setting(self, src_name, dst_name, options, output_settings):
+        value = self.get_option(src_name, options)
+        if value:
+            output_settings[dst_name] = value
+
+
+    def get_option(self, name, options, expand=False, default=None):
+        if not name in options:
+            return default
+        result = options[name]
+        if expand:
+            result = sublime.expand_variables(result, self.window.extract_variables())
+        return result
+
+## COMMANDS ##
+
+class EraseViewCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.erase(edit, sublime.Region(0, self.view.size()))
+
+class ChimneyCommand(sublime_plugin.WindowCommand):
+    def __init__(self, window):
+        super().__init__(window)
+        get_executor(window)
+
+    def run(self, **args):
+        executor = get_executor(self.window)
+        executor.run(**args)
+
+## STARTUP ##
+
+_executors = {}
+
+def get_executor(window):
+    if not window and not hasattr(window, 'id'):
+        raise ValueError("invalid Window object")
+
+    wid = window.id()
+    if not wid in _executors:
+        _executors[wid] = Executor(window)
+
+    return _executors[wid]
