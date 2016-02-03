768aa86b11a08486edd642cf7e64abb0c7c79e06
lastsecondsave <lastsecondsave@pm.me>
2016-02-03 22:08:07 +0300

    Match 'new' without a type nearby

---
+++ Sublemon/javascript_spec/syntax_test_javascript.js
@@ -0,0 +1,9 @@
+// SYNTAX TEST "Packages/Sublemon/javascript_spec/javascript.sublime-syntax"
+
+var a = new Object();
+//      ^ keyword.control.javascript
+//          ^ support.class.javascript
+
+var b = new factory.object();
+//      ^ keyword.control.javascript
+//          ^ source.js
