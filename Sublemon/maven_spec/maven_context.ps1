1deba15a87c9f5b52ca020047c318b4e2790aea4
lastsecondsave <lastsecondsave@outlook.com>
2016-02-05 01:11:10 +0300

    Migrate maven build to chimney

--- Sublemon/maven_spec/maven_context.ps1
+++
@@ -1,6 +0,0 @@
-filter MavenFilter() {
-    if ($_ -match '\[INFO\] -+') { return }
-    if ($_ -match '\[ERROR\] Failed to execute goal.*') { $skip = $true}
-    if ($skip -and $_ -match '\[[EIW]\w+\].*') { return }
-    $_
-}
