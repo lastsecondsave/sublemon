5779212008a7a0125c76cf7004049ef6b3ea948d
lastsecondsave <lastsecondsave@outlook.com>
2016-09-15 11:57:33 +0300

    Update publish script for Linux

---
+++ Sublemon/utils/publish_settings.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+set -e
+
+publish() {
+  pushd $(dirname $1)
+  echo "Running $(tput setaf 3)$(basename $1)$(tput sgr0) in $(tput setaf 2)$PWD$(tput sgr0)\n"
+  python $(basename $1)
+  echo
+  popd
+}
+
+pushd "$(dirname $0)/../.."
+
+for spy in $(find Sublemon -name '*.settings.py'); do
+  publish $spy
+done;
+
+publish "Disco/src/disco.py"
+
+popd
