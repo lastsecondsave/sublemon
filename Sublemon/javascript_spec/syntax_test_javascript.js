d5cbe3f8a0bf844add9559014c85d896fd336f0c
lastsecondsave <lastsecondsave@pm.me>
2016-09-30 00:44:53 +0300

    Return back to vanilla JavaScript package

--- Sublemon/javascript_spec/syntax_test_javascript.js
+++
@@ -1,9 +0,0 @@
-// SYNTAX TEST "Packages/Sublemon/javascript_spec/javascript.sublime-syntax"
-
-var a = new Object();
-//      ^ keyword.control.javascript
-//          ^ support.class.javascript
-
-var b = new factory.object();
-//      ^ keyword.control.javascript
-//          ^ source.js
