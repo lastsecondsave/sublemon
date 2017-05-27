16c31a1fc4b480d5057d3c08371467b48f177e89
lastsecondsave <lastsecondsave@pm.me>
2017-05-27 17:21:47 +0300

    Clean up build command for Powershell

--- Sublemon/builds.py
+++ Sublemon/builds.py
@@ -1,11 +1,8 @@
 from Sublemon.chimney import Pipe, ChimneyCommand
 
-import os
 
 class PowershellCommand(ChimneyCommand):
-    def preprocess_options(self, options, variables):
-        script = options["script"]
+    def preprocess_options(self, options):
+        script = options['script']
         if script:
-            if not os.path.isabs(script):
-                script = os.path.join(variables["folder"], script)
             options.shell_cmd = "&'{}'".format(script)
