3f0dbc6ad5e20bc75ba9689d8f1fd31cbce935ed
lastsecondsave <lastsecondsave@outlook.com>
2017-05-27 16:18:05 +0300

    Update Git commands to use new Chimney

--- Sublemon/git.py
+++ Sublemon/git.py
@@ -1,19 +1,45 @@
-from Sublemon.chimney import ChimneyCommand
+from Sublemon.chimney import ChimneyCommand, Pipe
+
 
 class GitDiffCommand(ChimneyCommand):
-    def preprocess_options(self, options, variables):
-        options.shell_cmd = "git diff -- '{}'".format(variables["file"])
+    def preprocess_options(self, options):
+        options.shell_cmd = "git diff -- '{}'".format(self.source_file())
         options.syntax = "Packages/Diff/Diff.tmLanguage"
 
+
+class GitLogPipe(Pipe):
+    def __init__(self):
+        self.lines = []
+        self.max_padding = 0
+
+    def output(self, line):
+        self.lines.append(line)
+        self.max_padding = max(line.find('→'), self.max_padding)
+
+    def flush(self):
+        for line in self.lines:
+            padding = line.find('→')
+            line = (line[:padding-1] +
+                    ' ' * (self.max_padding - padding) +
+                    line[padding-1:])
+            self.next_pipe.output(line)
+        self.next_pipe.flush()
+
+
 class GitLogCommand(ChimneyCommand):
-    def preprocess_options(self, options, variables):
-        options.shell_cmd = "git log -50 --follow --no-merges --date=short --format='{}' -- '{}'"\
-                .format("%h %ad %an → %s", variables["file"])
+    def create_pipe(self, options):
+        return GitLogPipe()
+
+    def preprocess_options(self, options):
+        template = "git log -50 --follow --no-merges --date=short --format='{}' -- '{}'"
+
+        options.shell_cmd = template.format("%h %ad %an → %s", self.source_file())
         options.syntax = "Packages/Sublemon/git_spec/git_log.sublime-syntax"
         options.scroll_to_end = False
 
+
 class GitBlameCommand(ChimneyCommand):
-    def preprocess_options(self, options, variables):
+    def preprocess_options(self, options):
         cmd = "git blame --date=short"
         view = self.window.active_view()
         sel = view.sel()[0]
@@ -26,5 +52,5 @@ class GitBlameCommand(ChimneyCommand):
 
             cmd += " -L {},{}".format(from_line, to_line)
 
-        options.shell_cmd = cmd + " -- '{}'".format(variables["file"])
+        options.shell_cmd = cmd + " -- '{}'".format(self.source_file())
         options.syntax = "Packages/Sublemon/git_spec/git_blame.sublime-syntax"
