4d05060308b5b938eb1927678491297f38a687d9
lastsecondsave <lastsecondsave@pm.me>
2018-10-08 23:31:15 +0800

    Simplify chimney a little bit

--- Sublemon/chimney.py
+++ Sublemon/chimney.py
@@ -1,8 +1,9 @@
-import collections
 import copy
 import os
+import signal
 import subprocess
 
+from collections import deque
 from threading import Lock, Thread
 
 import sublime
@@ -15,80 +16,33 @@ if RUNNING_ON_WINDOWS:
     STARTUPINFO.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 
 
-class Pipe(object):
-    def output(self, line):
-        self.next_pipe.output(line)
-
-    def error(self, line):
-        self.next_pipe.error(line)
-
-    def flush(self):
-        self.next_pipe.flush()
-
-    def close(self):
-        pass
-
-    def attach(self, next_pipe):
-        link_point = self
-        while hasattr(link_point, 'next_pipe'):
-            link_point = link_point.next_pipe
-        link_point.next_pipe = next_pipe
-
-
-class CleanEmptyLinesPipe(Pipe):
-    def __init__(self):
-        self.output_was_empty = False
-        self.error_was_empty = False
-
-    def output(self, line):
-        if line.strip():
-            if self.output_was_empty:
-                self.output_was_empty = False
-                self.next_pipe.output('')
-            self.next_pipe.output(line)
-        else:
-            self.output_was_empty = True
-
-    def error(self, line):
-        if line.strip():
-            if self.error_was_empty:
-                self.error_was_empty = False
-                self.next_pipe.error('')
-            self.next_pipe.error(line)
-        else:
-            self.error_was_empty = True
-
-
-class Buffer(object):
-    def __init__(self, output, encoding='utf-8'):
-        self.buffer = []
-        self.encoding = encoding
+class OutputBuffer:
+    def __init__(self, output):
+        self.buffer = deque()
         self.output = output
 
     def write(self, chunk):
-        try:
-            chunk = chunk.decode(self.encoding)
-        except:
-            chunk = "[DECODE ERROR]"
+        chunk = chunk.decode('utf-8')
 
-        b = 0
-        e = chunk.find('\n')
+        bgn = 0
+        end = chunk.find('\n')
 
-        while e != -1:
-            self.append_to_buffer(chunk, b, e)
+        while end != -1:
+            self.append(chunk, bgn, end)
             self.flush()
 
-            b = e + 1
-            e = chunk.find('\n', b)
+            bgn = end + 1
+            end = chunk.find('\n', bgn)
 
-        self.append_to_buffer(chunk, b, len(chunk))
+        self.append(chunk, bgn, len(chunk))
 
-    def append_to_buffer(self, chunk, b, e):
-        while e > 0 and chunk[e-1] == '\r':
-            e = e - 1
-        line = chunk[b:e]
-        if line:
-            self.buffer.append(line)
+    def append(self, chunk, bgn, end):
+        while end > 0 and chunk[end-1] == '\r':
+            end = end - 1
+
+        chunk = chunk[bgn:end]
+        if chunk:
+            self.buffer.append(chunk)
 
     def flush(self):
         if self.buffer:
@@ -97,20 +51,21 @@ class Buffer(object):
 
 
 class AsyncStreamConsumer(Thread):
-    def __init__(self, stream, output, on_close=None):
+    def __init__(self, stream, output_buffer, on_close=None):
         super().__init__()
         self.stream = stream
-        self.output = output
+        self.output_buffer = output_buffer
         self.on_close = on_close
 
     def run(self):
+        fileno = self.stream.fileno()
         while True:
-            chunk = os.read(self.stream.fileno(), 2**15)
+            chunk = os.read(fileno, 2**16)
             if not chunk:
                 break
-            self.output.write(chunk)
+            self.output_buffer.write(chunk)
 
-        self.output.flush()
+        self.output_buffer.flush()
         self.stream.close()
 
         if self.on_close:
@@ -123,30 +78,30 @@ class OutputPanel:
         self.view = window.create_output_panel('exec')
 
         self.line_buffer_lock = Lock()
-        self.line_buffer = collections.deque()
+        self.line_buffer = deque()
 
-        self.set_settings(gutter='False', scroll_past_end='False')
+        self.scroll_to_end = True
 
-    def reset(self):
-        self.view.run_command('erase_view')
-        self.line_buffer.clear()
+    def reset(self,
+              syntax=None, scroll_to_end=True,
+              **settings):
+        view_settings = self.view.settings()
 
-    def set_settings(self,
-                     syntax=None, scroll_to_end=False, show_on_text=False,
-                     **settings):
-        for k, v in settings.items():
-            if v:
-                self.view.settings().set(k, v)
+        view_settings.set('gutter', False)
+        view_settings.set('scroll_past_end', False)
+
+        for key, val in settings.items():
+            if val:
+                view_settings.set(key, val)
 
         if syntax:
             self.view.assign_syntax(syntax)
 
-        self.show_on_text = show_on_text
         self.scroll_to_end = scroll_to_end
 
         self.window.create_output_panel('exec')
 
-    def append_line(self, line):
+    def append(self, line):
         with self.line_buffer_lock:
             invalidate = len(self.line_buffer) == 0
             self.line_buffer.append(line)
@@ -156,7 +111,7 @@ class OutputPanel:
 
     def paint(self):
         with self.line_buffer_lock:
-            if len(self.line_buffer) == 0:
+            if not self.line_buffer:
                 return
             lines = copy.copy(self.line_buffer)
             self.line_buffer.clear()
@@ -167,43 +122,24 @@ class OutputPanel:
             'scroll_to_end': self.scroll_to_end
         })
 
-        if self.show_on_text:
-            self.show()
-            self.show_on_text = False
-
     def show(self):
-        self.window.run_command("show_panel", {"panel": "output.exec"})
+        self.window.run_command('show_panel', {'panel': 'output.exec'})
 
 
-class OutputPanelPipe(Pipe):
-    def __init__(self, output_panel):
-        self.output_panel = output_panel
+class Options:
+    def __init__(self, options, window):
+        self.originals = options
 
-    def output(self, line):
-        self.output_panel.append_line(line)
+        self.kill = self.get('kill', False)
+        self.cmd = self.get('cmd')
+        self.shell_cmd = self.get('shell_cmd')
+        self.file_regex = self.get('file_regex', '')
+        self.line_regex = self.get('line_regex', '')
+        self.syntax = self.get('syntax', 'Packages/Text/Plain text.tmLanguage')
+        self.scroll_to_end = self.get('scroll_to_end', True)
+        self.working_dir = self.get('working_dir')
 
-    def error(self, line):
-        self.output_panel.append_line(line)
-
-    def flush(self):
-        pass
-
-
-class Options(object):
-    def __init__(self, options_dict):
-        self.originals = options_dict
-
-        def option(x, y=None): return options_dict.get(x, y)
-
-        self.kill          = option("kill", False)
-        self.cmd           = option("cmd")
-        self.shell_cmd     = option("shell_cmd")
-        self.file_regex    = option("file_regex", "")
-        self.line_regex    = option("line_regex", "")
-        self.syntax        = option("syntax", "Packages/Text/Plain text.tmLanguage")
-        self.working_dir   = option("working_dir")
-        self.show_output   = option("show_output", "text")
-        self.scroll_to_end = option("scroll_to_end", True)
+        self.source_file = window.extract_variables().get('file')
 
     def get(self, arg, default=None):
         return self.originals.get(arg, default)
@@ -212,111 +148,110 @@ class Options(object):
         return self.get(arg)
 
 
-class Executor(object):
+class ExecutorContext:
+    def __init__(self, executor, options, process, listener):
+        self.window = executor.window
+        self.executor = executor
+        self.options = options
+        self.process = process
+        self.listener = listener
+        self.completed = False
+
+    def print(self, line):
+        self.executor.output_panel.append(line)
+
+    def complete(self):
+        self.window.status_message('Build finished')
+        self.listener.on_complete(self)
+
+        self.process.poll()
+
+        if self.process.returncode:
+            _log('✗ [{}] {}', self.process.pid, self.process.returncode)
+        else:
+            _log('✓ [{}]', self.process.pid)
+
+        self.completed = True
+
+
+class Executor:
     def __init__(self, window):
         self.window = window
         self.output_panel = OutputPanel(window)
-        self.running_state = None
-        _log('Created executor for window #{}', window.id())
+        self.ctx = None
 
-    def run(self, options, pipe):
-        if self.running_state:
+    def run(self, options, listener):
+        if self.ctx and not self.ctx.completed:
             self.kill_process()
 
         if options.kill:
             return
 
-        self.set_working_dir(options)
-        proc = self.start_process(options)
+        working_dir = self.get_working_dir(options.working_dir)
 
-        output_panel_settings = dict(
-            result_base_dir   = options.working_dir,
-            result_file_regex = options.file_regex,
-            result_line_regex = options.line_regex,
-            syntax            = options.syntax,
-            scroll_to_end     = options.scroll_to_end,
-            show_on_text      = options.show_output == "text"
+        self.output_panel.reset(
+            syntax=options.syntax,
+            scroll_to_end=options.scroll_to_end,
+            result_base_dir=working_dir,
+            result_file_regex=options.file_regex,
+            result_line_regex=options.line_regex
         )
 
-        self.output_panel.reset()
-        self.output_panel.set_settings(**output_panel_settings)
+        process = self.start_process(options, working_dir)
+        self.ctx = ExecutorContext(self, options, process, listener)
 
-        if options.show_output == "always":
-            self.output_panel.show()
+        listener.on_startup(self.ctx)
 
-        output_pipe = OutputPanelPipe(self.output_panel)
+        output_buffer = OutputBuffer(lambda line: listener.on_output(line, self.ctx))
+        error_buffer = OutputBuffer(lambda line: listener.on_error(line, self.ctx))
 
-        if pipe:
-            pipe.attach(output_pipe)
-        else:
-            pipe = output_pipe
+        AsyncStreamConsumer(process.stdout, output_buffer,
+                            on_close=self.ctx.complete).start()
+        AsyncStreamConsumer(process.stderr, error_buffer).start()
 
-        AsyncStreamConsumer(proc.stdout, Buffer(pipe.output), on_close=self.finish).start()
-        AsyncStreamConsumer(proc.stderr, Buffer(pipe.error)).start()
+        self.output_panel.show()
 
-        self.running_state = {'proc': proc, 'options': options, 'pipe': pipe}
-
-    def set_working_dir(self, options):
-        if not options.working_dir:
+    def get_working_dir(self, working_dir):
+        if not working_dir:
             view = self.window.active_view()
             if view and view.file_name():
-                options.working_dir = os.path.dirname(view.file_name())
-            return
+                return os.path.dirname(view.file_name())
 
-        if not os.path.isabs(options.working_dir):
+        if not os.path.isabs(working_dir):
             base_path = self.window.extract_variables().get('project_path')
-            options.working_dir = os.path.join(base_path, options.working_dir)
+            working_dir = os.path.join(base_path, working_dir)
 
-    def start_process(self, options):
+        return working_dir
+
+    def start_process(self, options, working_dir):
         if options.shell_cmd and RUNNING_ON_WINDOWS:
-            options.cmd = ["powershell.exe", "-Command", options.shell_cmd]
+            options.cmd = ['powershell.exe', '-Command', options.shell_cmd]
         elif options.shell_cmd:
-            options.cmd = [os.environ["SHELL"], "-c", options.shell_cmd]
+            options.cmd = [os.environ['SHELL'], '-c', options.shell_cmd]
 
-        os.chdir(options.working_dir)
-        proc = subprocess.Popen(options.cmd,
-                                startupinfo=STARTUPINFO,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                stdin=subprocess.DEVNULL)
+        os.chdir(working_dir)
+        process = subprocess.Popen(options.cmd,
+                                   startupinfo=STARTUPINFO,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   stdin=subprocess.DEVNULL)
 
-        _log('→ [{}] {}', proc.pid, ' '.join(options.cmd))
-        return proc
+        _log('→ [{}] {}', process.pid, ' '.join(options.cmd))
+        self.window.status_message('Build started: ' + options.shell_cmd)
 
-    def finish(self):
-        pipe = self.running_state['pipe']
-        pipe.flush()
-
-        show_output = self.running_state['options'].show_output
-        errors_count = len(self.output_panel.view.find_all_results())
-
-        if show_output == 'error' and errors_count > 0:
-            self.output_panel.show()
-
-        proc = self.running_state['proc']
-        proc.poll()
-
-        if proc.returncode:
-            _log('✗ [{}] {}', proc.pid, proc.returncode)
-        else:
-            _log('✓ [{}]', proc.pid)
-
-        self.window.status_message('Build complete')
-        pipe.close()
-        self.running_state = None
+        return process
 
     def kill_process(self):
-        pid = str(self.running_state['proc'].pid)
-        _log("Killing {}", pid)
+        process = self.ctx.process
+
+        _log('Killing {}', process.pid)
 
         if RUNNING_ON_WINDOWS:
-            cmd = ["taskkill", "/PID", pid]
+            cmd = 'taskkill /T /F /PID {}'.format(process.pid)
             subprocess.Popen(cmd, startupinfo=STARTUPINFO)
-            return
-
-        subprocess.Popen(["kill", pid])
-        if self.running_state['options'].shell_cmd is not None:
-            subprocess.Popen(["pkill", "-P", pid])
+        else:
+            os.killpg(process.pid, signal.SIGTERM)
+            process.terminate()
 
 
 class EraseViewCommand(TextCommand):
@@ -324,53 +259,55 @@ class EraseViewCommand(TextCommand):
         self.view.erase(edit, sublime.Region(0, self.view.size()))
 
 
+class ChimneyCommandListener:
+    def on_startup(self, ctx):
+        pass
+
+    def on_output(self, line, ctx):
+        ctx.print(line)
+
+    def on_error(self, line, ctx):
+        ctx.print(line)
+
+    def on_complete(self, ctx):
+        pass
+
+
 class ChimneyCommand(WindowCommand):
     def __init__(self, window):
         super().__init__(window)
         _get_executor(window)
 
-    def create_pipe(self, options):
-        pass
+    def get_listener(self):
+        return ChimneyCommandListener()
 
     def preprocess_options(self, options):
         pass
-
-    def source_file(self):
-        return self.window.extract_variables()['file']
-
-    def startup_message(self, options):
-        return 'Build started'
 
     def run(self, **kwargs):
         if 'update_phantoms_only' in kwargs:
             return
 
-        options = Options(kwargs)
+        options = Options(kwargs, self.window)
         self.preprocess_options(options)
 
-        pipe = self.create_pipe(options)
-
-        self.window.status_message(self.startup_message(options))
-
-        _get_executor(self.window).run(options, pipe)
+        _get_executor(self.window).run(options, self.get_listener())
 
 
 class ExecCommand(ChimneyCommand):
     pass
 
 
-_executors = {}
+_EXECUTORS = {}
 
 
 def _get_executor(window):
-    if not window and not hasattr(window, 'id'):
-        raise ValueError("invalid Window object")
-
     wid = window.id()
-    if wid not in _executors:
-        _executors[wid] = Executor(window)
+    if wid not in _EXECUTORS:
+        _EXECUTORS[wid] = Executor(window)
+        _log('Created executor for window #{}', wid)
 
-    return _executors[wid]
+    return _EXECUTORS[wid]
 
 
 def _log(message, *params):
--- Sublemon/maven.py
+++ Sublemon/maven.py
@@ -3,15 +3,12 @@ import re
 
 import sublime
 
-from Sublemon.chimney import Pipe, ChimneyCommand
-
-
-RUNNING_ON_WINDOWS = sublime.platform() == 'windows'
+from Sublemon.chimney import ChimneyCommand, ChimneyCommandListener, RUNNING_ON_WINDOWS
 
 DASHES_PATTERN = re.compile(r'\[INFO\] -+$')
 COMPILATION_FAILURE_PATTERN = re.compile(r'\[ERROR\] Failed to execute goal.*Compilation failure')
 SKIPPED_LINES_PATTERN = re.compile(r'\[[EIW]\w+\].*')
-DRIVE_LETTER_PATTERN  = re.compile(r'\[(?:ERROR|WARNING)\] /[A-Z]:')
+DRIVE_LETTER_PATTERN = re.compile(r'\[(?:ERROR|WARNING)\] /[A-Z]:')
 
 STATUS_PATTERN = re.compile(r'\[INFO\] BUILD (FAILURE|SUCCESS)$')
 TIME_PATTERN = re.compile(r'\[INFO\] Total time:')
@@ -19,14 +16,47 @@ TIME_PATTERN = re.compile(r'\[INFO\] Total time:')
 FILE_REGEX = r'^\[ERROR\] (\S.*):\[(\d+),(\d+)\](?: error:)? (.*)'
 
 
-class MavenPipe(Pipe):
-    def __init__(self, window):
-        self.window = window
+class MavenCommand(ChimneyCommand):
+    def preprocess_options(self, options):
+        if not options.working_dir:
+            variables = self.window.extract_variables()
+            is_pom = variables['file_name'] == 'pom.xml'
+            options.working_dir = variables['file_path' if is_pom else 'folder']
+
+        cmd = []
+
+        cmd.append('mvn -B')
+
+        if options['offline']:
+            cmd.append('-o')
+
+        if options['no_log']:
+            cmd.append('-q')
+            cmd.append('-e')
+
+        if options['mvn_global_opts']:
+            cmd.append(options['mvn_global_opts'])
+
+        if options['mvn_opts']:
+            cmd.append(options['mvn_opts'])
+
+        cmd.append(options['mvn_cmd'])
+
+        options.shell_cmd = ' '.join(cmd)
+        options.syntax = 'Packages/Sublemon/maven_spec/maven_build.sublime-syntax'
+        options.file_regex = FILE_REGEX
+
+    def get_listener(self):
+        return MavenCommandListener()
+
+
+class MavenCommandListener(ChimneyCommandListener):
+    def __init__(self):
         self.skip = False
         self.status = None
         self.time = None
 
-    def output(self, line):
+    def on_output(self, line, ctx):
         if COMPILATION_FAILURE_PATTERN.match(line):
             self.skip = True
 
@@ -47,51 +77,9 @@ class MavenPipe(Pipe):
             if match:
                 line = line[:match.end()-1] + line[match.end():]
 
-        self.next_pipe.output(line)
+        ctx.print(line)
 
-    def close(self):
+    def on_complete(self, ctx):
         if self.status:
             info = (self.status, self.time)
-            self.window.status_message(', '.join(info))
-
-
-class MavenCommand(ChimneyCommand):
-    def create_pipe(self, options):
-        return MavenPipe(self.window)
-
-    def preprocess_options(self, options):
-        if options.working_dir is None:
-            variables = self.window.extract_variables()
-            is_pom = variables['file_name'] == 'pom.xml'
-            options.working_dir = variables['file_path' if is_pom else 'folder']
-
-        cmd = []
-
-        if os.path.exists(os.path.join(options.working_dir, '.jenv-version')):
-            cmd.append('jenv exec')
-
-        cmd.append('mvn -B')
-
-        if options['offline']:
-            cmd.append('-o')
-
-        if options['quiet']:
-            cmd.append('-q')
-
-        if options['errors']:
-            cmd.append('-e')
-
-        if options['mvn_global_opts']:
-            cmd.append(options['mvn_global_opts'])
-
-        if options['mvn_opts']:
-            cmd.append(options['mvn_opts'])
-
-        cmd.append(options['mvn_cmd'])
-
-        options.shell_cmd = ' '.join(cmd)
-        options.syntax = 'Packages/Sublemon/maven_spec/maven_build.sublime-syntax'
-        options.file_regex = FILE_REGEX
-
-    def startup_message(self, options):
-        return 'Started ' + options.shell_cmd
+            ctx.window.status_message(', '.join(info))
