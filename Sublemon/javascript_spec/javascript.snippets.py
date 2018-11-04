cacdab42aebef70c44e9eb8c1f5dfaa89aad0683
lastsecondsave <lastsecondsave@pm.me>
2018-11-05 01:44:01 +0800

    Tweak snippets

--- Sublemon/javascript_spec/javascript.snippets.py
+++ Sublemon/javascript_spec/javascript.snippets.py
@@ -2,16 +2,26 @@ import sys
 sys.path.append('../lib')
 from snippets import Snippets
 
+
 js = Snippets('source.js')
-bl = js.with_suffix(' {\n\t$0\n}')
+js_bl = js.with_braces()
+js_bls = js.with_same_line_braces()
 
-js.con = ('continue', 'continue;')
-js.this = ('this.x = x', 'this.$1 = $1;')
-js.log = ('console.log', 'console.log(${0:$SELECTION});')
+js.con = 'continue;'
+js.tt = ('this.x = x', 'this.$1 = $1;')
+js.log = 'console.log(${0:$SELECTION});'
 
-bl.fore = ('for each', 'for (let $1 of $2)')
-bl.fori = ('for i', 'for (let ${1:i} = 0; $1 < ${2:imax}; ${3:$1++})')
+js_bl['if'] = 'if ($1)'
+js_bl['else'] = 'else'
+js_bl['elif'] = 'else if ($1)'
+js_bl['switch'] = 'switch ($1)'
+js_bl['while'] = 'while ($1)'
 
-bl.ff = ('function', 'function ${1:run}($2)')
-bl.fa = ('anonymous function', 'function($1)')
-bl.fp = ('function (json)', '${1:run}: function($2)')
+js_bl['for'] = ('for () {}', 'for ($1)')
+js_bl.fore = (':each: for () {}', 'for (let $1 of $2)')
+js_bl.fori = (':i: for () {}', 'for (let ${1:i} = 0; $1 < ${2:imax}; ${3:$1++})')
+
+js_bl.ff = ('function () {}', 'function ${1:run}($2)')
+js_bl.ffa = (':anon: function () {}', 'function($1)')
+js_bls.fla = (':oneline: :anon: function () {}', 'function($1)')
+js_bl.ffj = (':json: function () {}', '${1:run}: function($2)')
