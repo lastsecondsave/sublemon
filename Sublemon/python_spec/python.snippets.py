f618f8fcf3c1e4ef2841f1e9e0fce84545f8a34d
lastsecondsave <lastsecondsave@pm.me>
2018-12-03 22:23:25 +0800

    Some Python snippets

--- Sublemon/python_spec/python.snippets.py
+++ Sublemon/python_spec/python.snippets.py
@@ -7,6 +7,8 @@ py = Snippets('source.python')
 py_sp = py.with_space()
 py_bl = py.with_suffix(':\n\t$0\n')
 
+py_sp.im = 'import'
+py_sp.fim = 'from $1 import'
 py_sp.ret = 'return'
 
 py.stm = '@staticmethod'
@@ -22,12 +24,13 @@ py_bl['class'] = 'class $1'
 py_bl.init = ('__init__()', 'def __init__(self$1)')
 
 py_bl['if'] = 'if $1'
+py['ife'] = 'if $1 else $0'
 py_bl['else'] = 'else'
 py_bl['elif'] = 'elif $1'
 py_bl['while'] = 'while $1'
 
 py_bl['for'] = ('for', 'for $1 in $2')
-py_bl.fori = (':range: for', 'for ${1:i} in xrange(${2:0}..${3:imax})')
+py_bl.fori = (':range: for', 'for ${1:i} in range(${2:0}, ${3:imax})')
 
 py.lc = (':list: comprehension', '[$1 for $2 in $3]')
 py.tc = (':tuple: comprehension', '($1 for $2 in $3)')
