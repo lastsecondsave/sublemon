6a583552d431851d7b77a9da1e1aae6aae3502d9
lastsecondsave <lastsecondsave@outlook.com>
2019-06-18 01:09:59 +0800

    Pycodestyle syntax

--- Sublemon/pytools.py
+++ Sublemon/pytools.py
@@ -2,12 +2,10 @@ import re
 
 from Sublemon.chimney import ChimneyCommand, ChimneyBuildListener
 
-LINE_PATTERN = re.compile(r'(.+:\d+:)(\d+)(: .*)')
-
 
 class PylintCommand(ChimneyCommand):
     def setup(self, ctx):
-        cmd = ['python', '-m', 'pylint', self.window.active_view().file_name()]
+        cmd = ['python', '-m', 'pylint', ctx.file_name()]
 
         if ctx.opt('disable'):
             cmd.append('--disable=' + ','.join(ctx.opt('disable')))
@@ -19,11 +17,13 @@ class PylintCommand(ChimneyCommand):
 
 
 class PylintBuildListener(ChimneyBuildListener):
+    LINE_PATTERN = re.compile(r'(.+:\d+:)(\d+)(: .*)')
+
     def __init__(self):
         self.rank = None
 
     def on_output(self, line, ctx):
-        match = LINE_PATTERN.match(line)
+        match = self.LINE_PATTERN.match(line)
         if match:
             col = int(match.group(2)) + 1
             line = match.group(1) + str(col) + match.group(3)
@@ -36,3 +36,18 @@ class PylintBuildListener(ChimneyBuildListener):
     def on_complete(self, ctx):
         if self.rank:
             ctx.window.status_message(self.rank)
+
+
+class PycodestyleCommand(ChimneyCommand):
+    def setup(self, ctx):
+        cmd = ['python',
+               '-m', 'pycodestyle',
+               ctx.file_name(),
+               '--max-line-length=120']
+
+        if ctx.opt('ignore'):
+            cmd.append('--ignore=' + ','.join(ctx.opt('ignore')))
+
+        ctx.set(cmd=cmd,
+                file_regex=r'(.+?):(\d+):(\d+): (.*)',
+                syntax='pycodestyle')
