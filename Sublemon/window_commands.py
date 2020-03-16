b756b78d45a7e3e20eb053f34098bf3bf47f8ca7
lastsecondsave <lastsecondsave@pm.me>
2020-03-16 15:06:43 +0800

    Show hint on open file command

--- Sublemon/window_commands.py
+++ Sublemon/window_commands.py
@@ -53,22 +53,23 @@ class OpenFilePathCommand(WindowCommand):
         if root:
             path = root + path[1:]
 
-        parent = os.path.dirname(path)
-        if not parent:
-            parent = '.'
+        parent = os.path.dirname(path) or '.'
+        parent_exists = os.path.exists(parent)
 
         if path.endswith('+'):
             path = path[:-1]
-            if os.path.exists(parent):
+            if not parent_exists:
                 os.makedirs(parent)
+                parent_exists = True
 
-        if os.path.exists(parent):
+        if parent_exists:
             self.window.open_file(path, sublime.ENCODED_POSITION)
         else:
             sublime.status_message("Directory " + parent + " doesn't exist")
 
     def run(self):
         self.window.show_input_panel("File Path:", '', self.on_done, None, None)
+        self.window.status_message("@ - project, # - temp")
 
 
 class CloseWithoutSavingCommand(WindowCommand):
