6bf84314e491ceb9d068f15e02900c3e2f71e371
lastsecondsave <lastsecondsave@pm.me>
2020-03-17 13:53:14 +0800

    Revive WSL build

--- Sublemon/wsl.py
+++ Sublemon/wsl.py
@@ -1,30 +1,21 @@
 import re
-import shlex
-
 from Sublemon.chimney import ChimneyCommand, ChimneyBuildListener
 
 ESCAPE_CHARACTER = re.compile(r'\x1b.*?\[\d*m')
 SHEBANG = re.compile(r'\s*#!(.+)')
+WIN_PATH = re.compile(r'([A-Za-z]):\\(.*)')
 
 
 class WslCommand(ChimneyCommand):
     def setup(self, ctx):
-        view = self.window.active_view()
+        for i, val in enumerate(ctx.cmd.args):
+            match = WIN_PATH.match(val)
+            if match:
+                ctx.cmd.args[i] = ("/{}/{}".format(match.group(1).lower(),
+                                                   match.group(2).replace('\\', '/')))
 
-        filename = view.file_name()
-        if not filename:
-            ctx.cancel_build("No file")
-
-        cmd = ['wsl']
-
-        match = SHEBANG.match(view.substr(view.line(0)))
-        if match:
-            cmd.extend(shlex.split(match.group(1)))
-
-        cmd.append("$(wslpath '{}')".format(filename))
-
-        ctx.set(cmd=cmd,
-                listener=WslBuildListener())
+        ctx.cmd.appendleft("wsl")
+        ctx.set(listener=WslBuildListener())
 
 
 class WslBuildListener(ChimneyBuildListener):
