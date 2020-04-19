d05103639fa3f9b169830fbe4a171448bf61ae14
lastsecondsave <lastsecondsave@outlook.com>
2020-04-19 13:46:03 +0800

    Clean up settings.py

--- Sublemon/settings/settings.py
+++ Sublemon/settings/settings.py
@@ -1,33 +1,26 @@
 from settings_lib import settings
 
 settings("meta.context.sublime-syntax entity.name.tag",
-    show_in_symbol_list=1,
-    show_in_indexed_symbol_list=1
-)
+         show_in_symbol_list=1,
+         show_in_indexed_symbol_list=1)
 
 settings("meta.variables.sublime-syntax entity.name.tag",
-    show_in_symbol_list=1,
-    show_in_indexed_symbol_list=1,
-    symbol_transformation='/.*/[var] $0/'
-)
+         show_in_symbol_list=1,
+         show_in_indexed_symbol_list=1,
+         symbol_transformation='/.*/[var] $0/')
 
 settings("source.ini",
-    line_comment = (';', '#')
-)
+         line_comment=(';', '#'))
 
 settings("source.ini entity.name.section",
-    show_in_symbol_list = 1
-)
+         show_in_symbol_list=1)
 
 settings("source.unix",
-    line_comment = '#'
-)
+         line_comment='#')
 
 settings("text.rfc entity.name.title",
-    show_in_symbol_list = 1
-)
+         show_in_symbol_list=1)
 
 settings("source.powershell",
-    line_comment = '#',
-    block_comment = ('<#', '#>')
-)
+         line_comment='#',
+         block_comment=('<#', '#>'))
