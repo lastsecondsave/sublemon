afe44276debfad653e7b1c7b8429bc61ab1d462d
lastsecondsave <lastsecondsave@outlook.com>
2020-04-26 02:09:58 +0800

    Drop unused snippets

--- Sublemon/snippets/etc.snippets.py
+++ Sublemon/snippets/etc.snippets.py
@@ -5,3 +5,7 @@ regexp = Snippets('source.regexp')
 regexp/'>' - ('atomic group', '(?>${0:$SELECTION})')
 regexp/'=' - ('lookahead', '(?=${0:$SELECTION})')
 regexp/':' - ('non-capturing group', '(?:${0:$SELECTION})')
+
+clj = Snippets('source.clojure')
+
+clj/'pl' - ('println', '(println $0)')
--- Sublemon/snippets/powershell.snippets.py
+++ Sublemon/snippets/powershell.snippets.py
@@ -2,17 +2,15 @@ from snippets_lib import *
 
 ps = Snippets('source.powershell')
 
-ps/blk/'if' - ('if', 'if ($1)')
+ps/'pl' - 'Write-Output $0'
+
+ps/blk/'if' - 'if ($1)'
 ps/blk/'else' - 'else'
-ps/bls/'iff' - ('if (one-line)', 'if ($1)')
-ps/'ife' - ('if-else (one-line)', 'if ($1) { $2 } else { $0 }')
 
-ps/blk/'begin' - 'begin'
-ps/blk/'process' - 'process'
-ps/blk/'end' - 'end'
+ps/blk/'bg' - 'begin'
+ps/blk/'pr' - 'process'
+ps/blk/'en' - 'end'
+ps/blk/'fn' - 'function ${1:run}'
 
-ps/blp/'param' - 'param'
+ps/blp/'pm' - 'param'
 
-ps/blk/'fun' - 'function ${1:run}'
-
-ps/'pm' - ('Parameter', '[Parameter($1)]')
