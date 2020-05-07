1109eb976ac6aca44082498d7de2b40fff5099f6
lastsecondsave <lastsecondsave@outlook.com>
2020-05-07 21:58:15 +0800

    Move OpenFilePath command to separate file

--- Sublemon/window_commands.py
+++ Sublemon/window_commands.py
@@ -1,10 +1,7 @@
 import os
-import tempfile
 
 import sublime
 from sublime_plugin import TextInputHandler, WindowCommand
-
-from . import RUNNING_ON_WINDOWS
 
 HOME_PATH = os.path.expanduser('~')
 
@@ -32,44 +29,6 @@ class ShowFilePathCommand(WindowCommand):
 
         file_path = prefix + file_path if prefix else file_path
         sublime.status_message(file_path.replace(os.sep, ' / '))
-
-
-class OpenFilePathCommand(WindowCommand):
-    def on_done(self, path):
-        path = os.path.expandvars(path).strip()
-
-        if RUNNING_ON_WINDOWS:
-            path = path.replace('/', '\\')
-
-        root = path[0] if len(path) > 1 and path[1] == os.sep else None
-
-        if root == '~':
-            root = HOME_PATH
-        elif root == '@':
-            root = self.window.folders()[0]
-        elif root == '#':
-            root = tempfile.gettempdir()
-
-        if root:
-            path = root + path[1:]
-
-        parent = os.path.dirname(path) or '.'
-        parent_exists = os.path.exists(parent)
-
-        if path.endswith('+'):
-            path = path[:-1]
-            if not parent_exists:
-                os.makedirs(parent)
-                parent_exists = True
-
-        if parent_exists:
-            self.window.open_file(path, sublime.ENCODED_POSITION)
-        else:
-            sublime.status_message("Directory " + parent + " doesn't exist")
-
-    def run(self):
-        self.window.show_input_panel("File Path:", '', self.on_done, None, None)
-        self.window.status_message("@ - project, # - temp")
 
 
 class CloseWithoutSavingCommand(WindowCommand):
