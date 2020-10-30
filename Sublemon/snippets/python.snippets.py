b5c40bebe5463aa91aa84b877327000737bb685a
lastsecondsave <lastsecondsave@pm.me>
2020-10-30 19:59:52 +0300

    More snippets

--- Sublemon/snippets/python.snippets.py
+++ Sublemon/snippets/python.snippets.py
@@ -10,5 +10,6 @@ py/'self' - ('self.x = x', 'self.$1 = $1')
 
 py/blk/'def' - 'def ${1:run}($2)'
 py/blk/'init' - ('__init__', 'def __init__(self$1)')
+py/blk/'inm' - ('if __name__ == "__main__"', 'if __name__ == "__main__":')
 
 py/'pld' - '# pylint: disable='
