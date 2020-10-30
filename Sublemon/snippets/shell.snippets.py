fbe495248859994986932ccc0a5980d123a0ed31
lastsecondsave <lastsecondsave@outlook.com>
2020-10-30 19:59:52 +0300

    More snippets

--- Sublemon/snippets/shell.snippets.py
+++ Sublemon/snippets/shell.snippets.py
@@ -2,7 +2,9 @@ from snippets_lib import *
 
 sh = Snippets('source.shell')
 
+sh/'#!' - '#!/usr/bin/env bash'
 sh/'awk' - "awk '{$0}'"
 
 sh/ind/'case' - 'case $1 in>=>$0==>esac'
 sh/ind/'if' - 'if [[ $1 ]]; then>=>$0==>fi'
+sh/ind/'while' - 'while $1; do>=>$0==>done'
