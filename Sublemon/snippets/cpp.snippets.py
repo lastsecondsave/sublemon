b123f7e32fa08ad6bb23115e507c8c955c4d10d7
lastsecondsave <lastsecondsave@outlook.com>
2020-12-17 14:55:58 +0300

    More c++ snippets

--- Sublemon/snippets/cpp.snippets.py
+++ Sublemon/snippets/cpp.snippets.py
@@ -3,4 +3,9 @@ from snippets_lib import *
 cpp = Snippets('source.c++')
 
 cpp/'pl' - ('std::cout', r"std::cout << ${0:$SELECTION} << '\n';")
-cpp/'inc' - ('#include', '#include <$0>')
+
+cpp/ind/'dc' - ('doc comment', r'/**==>${SELECTION/^\s*/ * /mg}$0==> */')
+
+cpp/'inc' - ('#include', '#include $0')
+cpp/'#in' - ('#include', '#include $0')
+cpp/'ifdef' - ('#ifdef', '#ifdef $1\n${0:$SELECTION}\n#endif')
