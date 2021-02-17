5a453f9d34b41f64e6bc9a46b9e5019e6afe2fcb
lastsecondsave <lastsecondsave@pm.me>
2021-02-17 16:15:17 +0300

    Add cpplint

---
+++ Sublemon/cpptools.py
@@ -0,0 +1,31 @@
+from .chimney import ChimneyBuildListener, ChimneyCommand
+from .pytools import setup_python_exec
+
+
+class CpplintCommand(ChimneyCommand):
+    def setup(self, build):
+        setup_python_exec(build, "cpplint")
+
+        build.file_regex = r"(.+?):(\d+)(): (.*)"
+        build.syntax = "cpplint"
+        build.listener = CpplintBuildListener()
+
+
+class CpplintBuildListener(ChimneyBuildListener):
+    def __init__(self):
+        self.output_lines = [""]
+
+    def on_output(self, line, ctx):
+        if not line.startswith("Done processing"):
+            self.output_lines.append(line)
+
+    def on_error(self, line, ctx):
+        if line.startswith(ctx.working_dir):
+            line = line[len(ctx.working_dir) + 1 :]
+
+        line = line.replace("  ", " ")
+
+        return line
+
+    def on_complete(self, ctx):
+        ctx.print_lines(self.output_lines)
