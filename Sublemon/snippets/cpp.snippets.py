aa42c7138b395f0ee457c7adfcbdc7033fc70a73
lastsecondsave <lastsecondsave@outlook.com>
2021-05-06 02:38:50 +0300

    Improvements in new snippet generator

--- Sublemon/snippets/cpp.snippets.py
+++ Sublemon/snippets/cpp.snippets.py
@@ -1,4 +1,4 @@
-from snippets import *
+from snippets import Icon, generate
 
 snippets = {
     "pl": "std::cout << $SEL0 << std::endl;",
@@ -6,27 +6,41 @@ snippets = {
 }
 
 completions = {
-    kind(ICON_KEYWORD, "Keyword"): [
+    ("Keyword", Icon.KEYWORD): [
         "break",
         "continue",
         "return",
     ],
-    kind(ICON_CONVERT, "Cast"): [
+    ("Modifier", Icon.MODIFIER): [
+        "const",
+        "private",
+        "protected",
+        "public",
+    ],
+    ("Cast", Icon.CONVERT): [
         "const_cast<$1>",
         "reinterpret_cast<$1>",
         "static_cast<$1>",
     ],
-    kind(ICON_BLOCK, "Block"): [
-        "else ($1) {}",
+    ("Block", Icon.BLOCK): [
+        "else {}",
         "for ($1) {}",
         "if ($1) {}",
         "while ($1) {}",
     ],
-    kind(ICON_MACROS, "Macros"): [
+    ("Macros", Icon.META): [
         "#include",
         "#ifdef",
         "#endif",
     ],
+    ("Namespace", Icon.NAMESPACE): [
+        "std::"
+    ],
+    ("Storage", Icon.STORAGE): [
+        "class ${1:Class} {}",
+        "namespace ${1:ns} {}",
+        "struct ${1:Struct} {}",
+    ]
 }
 
 generate("source.c++", snippets, completions)
