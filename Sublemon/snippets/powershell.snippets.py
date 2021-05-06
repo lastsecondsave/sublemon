f8d9850a4e9675f6c287dfd17e8112557e7c2a6e
lastsecondsave <lastsecondsave@pm.me>
2021-05-06 21:21:03 +0300

    Redo snippets for powershell

--- Sublemon/snippets/powershell.snippets.py
+++ Sublemon/snippets/powershell.snippets.py
@@ -1,41 +1,47 @@
-from snippets_lib import *
+import json
+import subprocess
 
-ps = Snippets('source.powershell')
+from snippets import Icon, generate
 
-ps/'pl' - 'Write-Output $0'
 
-ps/blk/'if' - 'if ($1)'
-ps/blk/'else' - 'else'
+def pwsh_cmdlets():
+    sources = [
+        "Microsoft.PowerShell.Core",
+        "Microsoft.PowerShell.Management",
+        "Microsoft.PowerShell.Utility",
+    ]
 
-ps/blk/'bg' - 'begin'
-ps/blk/'pr' - 'process'
-ps/blk/'en' - 'end'
-ps/blk/'fn' - 'function ${1:run}'
+    pipeline = [
+        "Get-Command",
+        f"where Source -IN {','.join(sources)}",
+        "select Name, Source",
+        "ConvertTo-Json",
+    ]
 
-ps/blp/'pm' - 'param'
+    cmd = ["pwsh", "-NoLogo", "-Command", " | ".join(pipeline)]
 
-with ps.completions() as cmp:
-    cmp.group(["function", "", "Cmdlet"],
-              "Copy-Item",
-              "ForEach-Object",
-              "Format-List",
-              "Format-Table",
-              "Get-ChildItem",
-              "Get-Content",
-              "Get-Location",
-              "Join-Path",
-              "Move-Item",
-              "New-Item",
-              "Pop-Location",
-              "Push-Location",
-              "Remove-Item",
-              "Resolve-Path",
-              "Select-Object",
-              "Select-String",
-              "Set-Location",
-              "Sort-Object",
-              "Tee-Object",
-              "Test-Path",
-              "Write-Error",
-              "Write-Host",
-              "Write-Output")
+    process = subprocess.run(cmd, encoding="utf-8", capture_output=True)
+
+    if process.returncode != 0:
+        print("Error: Can't query pwsh for completions.")
+        return []
+
+    return [entry["Name"] for entry in json.loads(process.stdout)]
+
+
+snippets = {"pl": "Write-Output $SEL0"}
+
+completions = {
+    ("Cmdlet", ""): pwsh_cmdlets(),
+    ("Block", Icon.BLOCK): [
+        "begin {}",
+        "else {}",
+        "end {}",
+        "function ${1:run} {}",
+        "if ($1) {}",
+        "process {}",
+    ],
+}
+
+generate("source.powershell", snippets, completions)
+
