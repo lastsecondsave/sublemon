9246110f1a2ddb39db1d230f8b1c88acc97dfa9a
lastsecondsave <lastsecondsave@outlook.com>
2021-05-07 01:54:33 +0300

    Convert Python snippets

--- Sublemon/snippets/python.snippets.py
+++ Sublemon/snippets/python.snippets.py
@@ -1,16 +1,59 @@
-from snippets_lib import *
+from snippets import DEFAULT_MUTATORS, Icon, generate
 
-def blk(s): return s + ':\n\t$0'
+snippets = {
+    "pl": "print($SEL0)",
+    "ss": ("self.x = x", "self.$1 = $1"),
+    "pld": ("pylint disable", "# pylint: disable="),
+    "main": ('if __name__ == "__main__"', 'if __name__ == "__main__":==>${0:main()}'),
+    "fi": ("from import", "from $1 import $0"),
+    "init": ("__init__", "def __init__(self$1):"),
+}
 
-py = Snippets('source.python')
+completions = {
+    ("Keyword", Icon.KEYWORD): [
+        "break",
+        "continue",
+        "from",
+        "import",
+        "pass",
+        "raise",
+        "return",
+        "yeild",
+    ],
+    ("Block", Icon.BLOCK): [
+        "elif $1:",
+        "else:",
+        "for $1 in $2:",
+        "if $1:",
+        "while $1:",
+    ],
+    ("Storage", Icon.STORAGE): [
+        "def ${1:run}($2):",
+        "class ${1:Item}($2):",
+    ],
+    ("Support", Icon.FUNCTION): [
+        "isinstance($1, ${2:str})",
+    ],
+    ("Decorator", Icon.META): [
+        "@classmethod",
+        "@dataclass",
+        "@staticmethod",
+    ],
+    ("Constant", Icon.CONSTANT): [
+        "False",
+        "None",
+        "True",
+    ],
+    ("Variable", Icon.VARIABLE): [
+        "self",
+    ],
+}
 
-py/'pl' - 'print(${0:$SELECTION})'
-py/'isi' - 'isinstance($1, ${2:str})'
 
-py/'self' - ('self.x = x', 'self.$1 = $1')
+def expand_colon(content):
+    return content + "\n\t${0:pass}" if content.endswith(":") else content
 
-py/blk/'def' - 'def ${1:run}($2)'
-py/blk/'init' - ('__init__', 'def __init__(self$1)')
-py/blk/'inm' - ('if __name__ == "__main__"', 'if __name__ == "__main__"')
 
-py/'pld' - '# pylint: disable='
+generate(
+    "source.python", snippets, completions, mutators=(*DEFAULT_MUTATORS, expand_colon)
+)
