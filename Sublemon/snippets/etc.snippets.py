1deb6ed1b27c8cbd1ba07bd36792cb34cdfaee85
lastsecondsave <lastsecondsave@pm.me>
2021-05-07 21:26:43 +0300

    Add some snippets for Makefile

--- Sublemon/snippets/etc.snippets.py
+++ Sublemon/snippets/etc.snippets.py
@@ -1,4 +1,4 @@
-from snippets import generate
+from snippets import Icon, generate
 
 generate(
     "source.regexp",
@@ -14,5 +14,29 @@ generate(
     snippets={
         "cdata": ("CDATA", "<![CDATA[$SEL0]]>"),
         "xml": ("xml declaration", '<?xml version="1.0" encoding="UTF-8"?>'),
+    },
+)
+
+generate(
+    "source.makefile",
+    completions={
+        ("Block", Icon.BLOCK): [
+            "define $1<->endef",
+            "ifdef $1<->endif",
+            'ifeq "$1" "$2"<->endif',
+        ]
+    },
+)
+
+generate(
+    "source.makefile meta.function-call",
+    completions={
+        ("Function", Icon.FUNCTION): [
+            "addprefix",
+            "addsuffix",
+            "call",
+            "patsubst",
+            "subst",
+        ]
     },
 )
