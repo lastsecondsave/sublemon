c68da4a4a86318371c52adf7eaedd55e0a15a8dd
lastsecondsave <lastsecondsave@pm.me>
2021-05-26 21:43:57 +0300

    Convert Java snippets and remove old generator

--- Sublemon/snippets/java.snippets.py
+++ Sublemon/snippets/java.snippets.py
@@ -1,77 +1,4 @@
-from snippets_lib import *
-
-java = Snippets('source.java')
-
-java/spc/'f' - 'final'
-java/spc/'st' - 'static'
-java/spc/'pr' - 'private'
-java/spc/'pu' - 'public'
-java/spc/'pro' - 'protected'
-java/spc/'th' - 'throws'
-
-java/spc/'im' - 'import'
-java/'ima' - ('import *', 'import $0.*;')
-
-java/'sf' - ('static final CONSTANT', 'static final ${1:String} ${2:CONSTANT} = ${3:null};')
-java/scl/'con' - 'continue'
-java/'this' - ('this.x = x', 'this.$1 = $1;')
-
-java/spc/'ret' - 'return'
-java/scl/'rtt' - 'return this'
-java/'te' - ('throw new Exception', 'throw new ${1:Exception}($2);')
-java/'tre' - 'throw new RuntimeException($1);'
-
-vv = r'${1:String} ${2:${1/./\l$0/}}'
-java/'vv' - ('variable', vv)
-java/'vvn' - ('variable (new)', vv + ' = new $1($3)')
-java/'vvv' - ('variable (null)', vv + ' = ${3:null}')
-
-java/'pl' - 'System.out.println(${0:$SELECTION});'
-java/'lgg' - ('logger', 'private static final Logger log = LoggerFactory.getLogger($FILENAME.class);')
-
-java/spc/'List' - 'List<${1:String}>'
-java/spc/'Set' - 'Set<${1:String}>'
-java/spc/'Map' - 'Map<${1:String}, ${2:String}>'
-
-java/spc/'op' - 'Optional<${1:String}>'
-java/slp/'opo' - 'Optional.of'
-java/slp/'opn' - 'Optional.ofNullable'
-java/'ope' - 'Optional.empty()'
-
-java/slp/'rnn' - 'Objects.requireNonNull'
-
-java/'over' - '@Override'
-java/'sw' - '@SuppressWarnings("${1:unchecked}")'
-
-java/'td' - ('TODO', '// TODO: ')
-
-java/blk/'if' - 'if ($1)'
-java/blk/'else' - 'else'
-java/blk/'elif' - 'else if ($1)'
-java/blk/'switch' - 'switch ($1)'
-java/blk/'while' - 'while ($1)'
-
-java/blk/'for' - 'for ($1)'
-java/blk/'fori' - ('for (i)', 'for (int ${1:i} = 0; $1 < ${2:imax}; ${3:$1++})')
-java/blk/'form' - ('for (imax)', 'for (int ${1:i} = 0, $1max = ${2:count}; $1 < $1max; ${3:$1++})')
-java/blk/'fore' - ('for (each)', 'for ($1 : $2)')
-java/blk/'fort' - ('for (iterator)', 'for (Iterator<$1> ${2:itr} = ${3:list}.iterator(); $2.hasNext(); )')
-
-java/blk/'try' - 'try'
-java/blk/'tryr' - ('try (with resources)', 'try ($1)')
-java/blk/'catch' - 'catch (${1:Exception} e)'
-java/blk/'finally' - 'finally'
-
-java/blk/'syn' - 'synchronized (${1:this})'
-
-java/blk/'class' - 'class ${1:$FILENAME}'
-java/blk/'inter' - 'interface ${1:$FILENAME}'
-java/blk/'enum' - 'enum ${1:$FILENAME}'
-
-java/blk/'ctor' - ('constructor', '$FILENAME($1)')
-java/blk/'fun' - ('method', '${1:void} ${2:run}($3)')
-java/blk/'main' - ('main', 'public static void main(String[] args)')
-java/blk/'tos' - ('toString', 'public String toString()')
+from snippets import Icon, generate
 
 getter = r"""
 public ${1:String} ${1/boolean|(.*)/(?1:get:is)/}${2/./\u$0/}() {
@@ -85,18 +12,110 @@ public void set${2/./\u$0/}(${1:String} ${2/.*/$0/}) {
 }
 """
 
-java/'get' - ('getter', getter)
-java/'geto' - ('getter (optional)', getter.replace('public ${1:String}', 'public Optional<${1:String}>'))
+snippets = {
+    "pl": "System.out.println($SEL0);",
+    "sf": (
+        "static final CONSTANT",
+        "static final ${1:String} ${2:CONSTANT} = ${3:null};",
+    ),
+    "ima": ("import *", "import $0.*;"),
+    "fun": ("function", "${1:void} ${2:run}($3) {}"),
+    "main": ("main", "public static void main(String[] args) {}"),
+    "tt": ("this.x = x", "this.$1 = $1;"),
+    "te": ("throw Exception", "throw new ${1:RuntimeException}($2);"),
+    "lgg": (
+        "logger",
+        "private static final Logger log = LoggerFactory.getLogger(${1:$FILENAME}.class);",
+    ),
+    "fori": ("for i", "for (int ${1:i} = 0; $1 < ${2:imax}; ${3:$1++}) {}"),
+    "fort": (
+        "for iterator",
+        "for (Iterator<$1> ${2:itr} = ${3:list}.iterator(); $2.hasNext(); ) {}",
+    ),
+    "jd": ("javadoc", r"/**-->${SELECTION/^\s*/ * /mg}$0--> */"),
+    "td": ("TODO", "// TODO: "),
+    "get": ("getter", getter),
+    "geto": (
+        "getter with optional",
+        getter.replace("public ${1:String}", "public Optional<${1:String}>"),
+    ),
+    "set": ("setter", setter),
+    "gs": ("getter + setter", getter + setter),
+    "rnn": "Objects.requireNonNull($SEL1)",
+}
 
-java/'set' - ('setter', setter)
+completions = {
+    ("Keyword", Icon.KEYWORD): [
+        "break",
+        "case",
+        "continue",
+        "return",
+        "throw",
+        "import",
+        "package",
+    ],
+    ("Modifier", Icon.ACCESS_MODIFIER): [
+        "final",
+        "private",
+        "protected",
+        "public",
+        "static",
+        "volatile",
+    ],
+    ("Block", Icon.BLOCK): [
+        "catch (${1:Exception} e) {}",
+        "else {}",
+        "finally {}",
+        "for ($1) {}",
+        "if ($1) {}",
+        "switch ($1) {}",
+        "synchronized (${1:this}) {}",
+        "try {}",
+        "while ($1) {}",
+        ("elif", "else if ($1) {}"),
+    ],
+    ("Storage", Icon.STORAGE): [
+        "class ${1:$FILENAME} {}",
+        "enum ${1:$FILENAME} {}",
+        "interface ${1:$FILENAME} {}",
+    ],
+    ("Annotation", Icon.META): [
+        "@Override",
+        "@SuppressWarnings()",
+    ],
+    ("Type", Icon.PRIMITIVE_TYPE): [
+        "boolean",
+        "byte",
+        "char",
+        "double",
+        "float",
+        "int",
+        "long",
+        "short",
+        "void",
+    ],
+    ("Class", Icon.TYPE): [
+        "Optional",
+        "String",
+    ],
+    ("Constant", Icon.CONSTANT): [
+        "false",
+        "null",
+        "true",
+    ],
+    ("Variable", Icon.VARIABLE): [
+        "super",
+        "this",
+    ],
+}
 
-java/'gs' - ('getter + setter', getter + setter)
+generate("source.java", snippets, completions)
 
-java/ind/'jd' - ('javadoc', r'/**==>${SELECTION/^\s*/ * /mg}$0==> */')
 
-javadoc = java.subscope('comment.block.documentation.javadoc')
-
-javadoc/'code' - ('@code', '{@code ${1:$SELECTION}}')
-javadoc/'link' - ('@link', '{@link ${1:$SELECTION}}')
-javadoc/spc/'par' - '@param'
-javadoc/spc/'ret' - '@return'
+generate(
+    "source.java comment.block.documentation.javadoc",
+    snippets={
+        "code": ("@code", "{@code $SEL1}"),
+        "link": ("@link", "{@link $SEL1}"),
+    },
+)
