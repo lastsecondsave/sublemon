077ef7285ae5ddd190b7ad8e96bd037749243ff3
lastsecondsave <lastsecondsave@pm.me>
2021-06-08 20:19:29 +0300

    Add prototype for highlight command

--- Sublemon/highlight.py
+++ Sublemon/highlight.py
@@ -1,5 +1,32 @@
+import sublime
 from sublime_plugin import ListInputHandler, TextCommand
 
 
 class HighlightAddCommand(TextCommand):
-    pass
+    def input(self, _args):
+        return StyleInputHandler()
+
+    def run(self, edit, style, color):  # pylint: disable=arguments-differ
+        regions = [r for r in self.view.sel() if not r.empty()]
+
+        flags = sublime.DRAW_NO_OUTLINE
+        if style == "Underline":
+            flags |= sublime.DRAW_SOLID_UNDERLINE | sublime.DRAW_NO_FILL
+
+        scope = f"style.{color.lower()}"
+        name = f"{color} {style}"
+
+        self.view.add_regions(name, regions, flags=flags, scope=scope)
+
+
+class StyleInputHandler(ListInputHandler):
+    def list_items(self):
+        return ["Highlight", "Underline"]
+
+    def next_input(self, _args):
+        return ColorInputHandler()
+
+
+class ColorInputHandler(ListInputHandler):
+    def list_items(self):
+        return ["Orange", "White", "Crimson", "Blue"]
