3cd0e15003ff3d12928ec8e36093754ef7b7a82c
lastsecondsave <lastsecondsave@outlook.com>
2021-06-11 19:30:30 +0300

    Add commands to remove highlights

--- Sublemon/highlight.py
+++ Sublemon/highlight.py
@@ -1,13 +1,22 @@
 import sublime
 from sublime_plugin import ListInputHandler, TextCommand
 
+SETTING_GROUPS = "sublemon.highlight.groups"
+
 
 class HighlightAddCommand(TextCommand):
     def input(self, _args):
         return StyleInputHandler()
 
+    def is_enabled(self):
+        return all(not r.empty() for r in self.view.sel())
+
     def run(self, edit, style, color):  # pylint: disable=arguments-differ
         regions = [r for r in self.view.sel() if not r.empty()]
+
+        for region in regions:
+            self.view.sel().subtract(region)
+            self.view.sel().add(sublime.Region(region.end(), region.end()))
 
         flags = sublime.DRAW_NO_OUTLINE
         if style == "Underline":
@@ -21,6 +30,12 @@ class HighlightAddCommand(TextCommand):
 
         self.view.add_regions(name, regions, flags=flags, scope=scope)
 
+        groups = self.view.settings().get(SETTING_GROUPS, [])
+        if name not in groups:
+            groups.append(name)
+
+        self.view.settings().set(SETTING_GROUPS, groups)
+
 
 class StyleInputHandler(ListInputHandler):
     def list_items(self):
@@ -33,3 +48,34 @@ class StyleInputHandler(ListInputHandler):
 class ColorInputHandler(ListInputHandler):
     def list_items(self):
         return ["Orange", "Dark Orange", "Crimson", "Blue", "Green", "Gray"]
+
+
+class HighlightDeleteAllCommand(TextCommand):
+    def is_enabled(self):
+        return bool(self.view.settings().get(SETTING_GROUPS))
+
+    def run(self, edit):
+        groups = self.view.settings().get(SETTING_GROUPS)
+        for group in groups:
+            self.view.erase_regions(group)
+        self.view.settings().erase(SETTING_GROUPS)
+
+
+class HighlightDeleteCommand(TextCommand):
+    def is_enabled(self):
+        return bool(self.view.settings().get(SETTING_GROUPS))
+
+    def run(self, edit, group):  # pylint: disable=arguments-differ
+        self.view.erase_regions(group)
+        groups = self.view.settings().get(SETTING_GROUPS)
+        groups.remove(group)
+        self.view.settings().set(SETTING_GROUPS, groups)
+
+    def input(self, _args):
+        groups = self.view.settings().get(SETTING_GROUPS)
+
+        class GroupInputHandler(ListInputHandler):
+            def list_items(self):
+                return groups
+
+        return GroupInputHandler()
