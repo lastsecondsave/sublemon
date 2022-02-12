c186489a62cb1ecebef40cfd91889c2538bb69e4
lastsecondsave <lastsecondsave@pm.me>
2022-02-12 22:00:25 +0300

    Move CMake snippets to its own file

---
+++ Sublemon/snippets/cmake.snippets.py
@@ -0,0 +1,42 @@
+from snippets import Icon, generate
+
+generate(
+    "source.cmake",
+    snippets={
+        "pl": "message($SEL0)",
+    },
+    completions={
+        ("Function", Icon.FUNCTION): [
+            "add_executable",
+            "add_library",
+            "add_subdirectory",
+            "target_compile_options",
+            "target_include_directories",
+            "target_link_directories",
+            "target_link_libraries",
+            "target_sources",
+        ],
+        ("Block", Icon.BLOCK): [
+            "function($1)<=>endfunction()",
+            "if($1)<=>endif()",
+            "macro($1)<=>endmacro()",
+        ],
+        ("Variable", Icon.VARIABLE): [
+            "CMAKE_BINARY_DIR",
+            "CMAKE_CURRENT_BINARY_DIR",
+            "CMAKE_CURRENT_SOURCE_DIR",
+            "CMAKE_SOURCE_DIR",
+        ],
+    },
+)
+
+generate(
+    "source.cmake meta.function-call",
+    completions={
+        ("Constant", Icon.ACCESS_MODIFIER): [
+            "PUBLIC",
+            "PRIVATE",
+            "INTERFACE",
+        ]
+    },
+)
--- Sublemon/snippets/etc.snippets.py
+++ Sublemon/snippets/etc.snippets.py
@@ -47,46 +47,6 @@ generate(
 )
 
 generate(
-    "source.cmake",
-    snippets={
-        "pl": "message($SEL0)",
-    },
-    completions={
-        ("Function", Icon.FUNCTION): [
-            "add_executable",
-            "add_library",
-            "add_subdirectory",
-            "target_include_directories",
-            "target_link_directories",
-            "target_link_libraries",
-            "target_sources",
-        ],
-        ("Block", Icon.BLOCK): [
-            "function($1)<=>endfunction()",
-            "if($1)<=>endif()",
-            "macro($1)<=>endmacro()",
-        ],
-        ("Variable", Icon.VARIABLE): [
-            "CMAKE_BINARY_DIR",
-            "CMAKE_CURRENT_BINARY_DIR",
-            "CMAKE_CURRENT_SOURCE_DIR",
-            "CMAKE_SOURCE_DIR",
-        ],
-    },
-)
-
-generate(
-    "source.cmake meta.function-call",
-    completions={
-        ("Constant", Icon.ACCESS_MODIFIER): [
-            "PUBLIC",
-            "PRIVATE",
-            "INTERFACE",
-        ]
-    },
-)
-
-generate(
     "source.rust",
     snippets={
         "pl": 'println!("{}", $SEL0);',
