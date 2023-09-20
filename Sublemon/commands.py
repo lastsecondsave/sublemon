99513c11823c98046c4db5c0ab9d34bd0fd65263
lastsecondsave <lastsecondsave@pm.me>
2023-09-20 21:38:27 +0000

    Tweak MoveViewCommand

--- Sublemon/commands.py
+++ Sublemon/commands.py
@@ -437,26 +437,6 @@ class SaveAllEdited(WindowCommand):
                 view.run_command("save")
 
 
-class MoveViewCommand(WindowCommand):
-    def run(self, forward=True):  # pylint: disable=arguments-differ
-        sheet = self.window.active_sheet()
-        group, index = self.window.get_sheet_index(sheet)
-
-        if forward:
-            index += 1
-            if index == len(self.window.sheets_in_group(group)):
-                group += 1
-                index = 0
-        else:
-            index -= 1
-            if index < 0:
-                group -= 1
-                index = len(self.window.sheets_in_group(group))
-
-        if 0 <= group < self.window.num_groups():
-            self.window.set_sheet_index(sheet, group, index)
-
-
 class MoveViewportHorizontallyCommand(TextCommand):
     def run(self, _edit, forward=True):  # pylint: disable=arguments-differ
         current = self.view.viewport_position()
--- Sublemon/tabber.py
+++ Sublemon/tabber.py
@@ -12,8 +12,32 @@ class TabberNextCommand(WindowCommand):
         self.window.focus_sheet(sheets[index])
 
 
-class TabberFocus(WindowCommand):
+class TabberFocusCommand(WindowCommand):
     def run(self, index):  # pylint: disable=arguments-differ
         sheets = self.window.selected_sheets()
         if index < len(sheets):
             self.window.focus_sheet(sheets[index])
+
+
+class TabberMoveCommand(WindowCommand):
+    def run(self, forward=True):  # pylint: disable=arguments-differ
+        selected_sheets = self.window.selected_sheets()
+        sheet = self.window.active_sheet()
+        group, index = self.window.get_sheet_index(sheet)
+
+        if forward:
+            index += 1
+            if index == len(self.window.sheets_in_group(group)):
+                group += 1
+                index = 0
+        else:
+            index -= 1
+            if index < 0:
+                group -= 1
+                index = len(self.window.sheets_in_group(group))
+
+        if 0 <= group < self.window.num_groups():
+            self.window.set_sheet_index(sheet, group, index)
+
+        if len(selected_sheets) > 1:
+            self.window.select_sheets(selected_sheets)
